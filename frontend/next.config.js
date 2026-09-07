313a3f0ddabf39a8c2bd0afae07d02437fc2a18b
Jaykrishna25 <itsjaykrishna@gmail.com>
2026-09-08 01:37:10 +0530

    Mark Prisma as external server package for Vercel runtime

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,22 +1,8 @@
-const path = require("path");
-
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-  outputFileTracingRoot: path.join(__dirname),
-  outputFileTracingExcludes: {
-    "*": [
-      "node_modules/@swc/**",
-      "node_modules/esbuild/**",
-      "node_modules/@esbuild/**",
-      "node_modules/terser/**",
-      "node_modules/sharp/**",
-      "node_modules/prisma/**",
-      "node_modules/@prisma/engines/**",
-    ],
-  },
+  serverExternalPackages: ["@prisma/client", "prisma"],
 };
 
 module.exports = nextConfig;
