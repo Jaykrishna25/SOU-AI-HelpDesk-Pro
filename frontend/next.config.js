67d564c1c73f32194d866ac797d6a79dfa6c99bc
Jaykrishna25 <itsjaykrishna@gmail.com>
2026-09-08 00:51:26 +0530

    Scope Next.js output file tracing to frontend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,20 @@
+const path = require("path");
+
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
+  outputFileTracingRoot: path.join(__dirname),
+  outputFileTracingExcludes: {
+    "*": [
+      "node_modules/@swc/**",
+      "node_modules/esbuild/**",
+      "node_modules/@esbuild/**",
+      "node_modules/terser/**",
+      "node_modules/sharp/**",
+    ],
+  },
 };
+
 module.exports = nextConfig;
